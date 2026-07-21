da4300a134d040b49d4fa8ac1a6d5f8792b3b698
Ice-rink <15551057621@163.com>
2026-07-21 11:56:05 +0000

    fix: 增加自动解析功能并优化音乐获取逻辑

--- sb3_getMusic/index.js
+++ sb3_getMusic/index.js
@@ -9,7 +9,11 @@ configFile.initFile("config.json", {
     private_all: true,
 
     cmd: "/music",
-    url: "https://www.yueting.net/tool/music/"
+    auto_analysis: true,
+
+    url: "https://www.yueting.net/tool/music/",
+    web_ua: "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/137.0.7120.0 Safari/537.36",
+    web_referer: true
 })
 
 // 网页配置
@@ -21,7 +25,11 @@ spark.web.createConfig("getMusic")
     .switch("private_all", config.private_all, "允许所有私聊")
 
     .text("cmd", config.cmd, "触发指令")
+    .switch("auto_analysis", config.auto_analysis, "自动解析url")
+
     .text("url", config.url, "请求端点")
+    .text("web_ua", config.web_ua, "请求UA (留空不请求)")
+    .switch("web_referer", config.web_referer, "启用Referer")
     .register();
 
 spark.on("config.update.getMusic", (key, val) => {
@@ -37,7 +45,8 @@ spark.on("config.update.getMusic", (key, val) => {
 spark.on('message.group.normal', (pack, reply) => {
     if (!((config.group_all
         || config.group.includes(pack.group_id))
-        && pack.raw_message.startsWith(config.cmd)
+        && (pack.raw_message.startsWith(config.cmd)
+        || (config.auto_analysis && pack.raw_message.includes("//music.163.com")))
     )) return;
 
     onMessage(pack, reply);
@@ -47,7 +56,8 @@ spark.on('message.group.normal', (pack, reply) => {
 spark.on('message.private.friend', (pack, reply) => {
     if (!((config.private_all
         || config.private.includes(pack.target_id))
-        && pack.raw_message.startsWith(config.cmd)
+        && (pack.raw_message.startsWith(config.cmd)
+        || (config.auto_analysis && pack.raw_message.includes("//music.163.com")))
     )) return;
 
     onMessage(pack, reply);
@@ -57,11 +67,19 @@ async function onMessage(pack, reply) {
     const cmd = pack.message
         .map(i => i.type === "text" ? i.data.text : "")
         .join("")
-        .slice(config.cmd.length + 1)
-        .split(" ");
 
+    let musicList = [];
+    if (config.auto_analysis && cmd.includes("music.163.com"))
+        musicList = await getMusic(cmd, "url");
+    else
+        musicList = await getMusic(
+            cmd
+                .slice(config.cmd.length + 1)
+                .split(" ")
+                .join(" ")
+        );
     // logger.info(JSON.stringify(cmd, null, 4))
-    const musicList = await getMusic(cmd.join(" "));
+    
     const replyMsg = [
         spark.msgbuilder.reply(pack.real_id)
     ];
@@ -93,9 +111,23 @@ spark.on("event.aichat.starts", () => {
                 parameters: {
                     type: "object",
                     properties: {
-                        query: {
+                        data: {
                             type: "string",
-                            description: "查询的音乐名称",
+                            description: "查询的音乐数据，名称或链接",
+                        },
+                        mode: {
+                            type: "string",
+                            enum: ["name", "url"],
+                            description: "模式",
+                        },
+                        type: {
+                            type: "string",
+                            description: "要查询的平台，默认'netease'(网易云)",
+                        },
+                        page: {
+                            "type": "integer",
+                            "description": "翻页页数",
+                            "minimum": 1,
                         },
                         lyrics: {
                             type: 'boolean',
@@ -103,13 +135,14 @@ spark.on("event.aichat.starts", () => {
                             default: false
                         }
                     },
-                    required: ["query", "lyrics"]
+                    required: ["data", "mode", "type", "page", "lyrics"]
                 }
             }
         },
-        call: async (chatData, query, lyrics) => {
-            const data = await getMusic(query);
-            return data.map(i => {
+        call: async (chatData, data, mode, type, page, lyrics) => {
+            const res = await getMusic(data, mode, type, page);
+            if (!res) return "未获取到相关内容，确定参数或名称是对的吗？";
+            return res.map(i => {
                 return {
                     '名称': i.title,
                     '作者': i.author,
@@ -124,25 +157,67 @@ spark.on("event.aichat.starts", () => {
 })
 
 
-async function getMusic(name) {
-    const data = await axios.post(config.url, {
-        input: name, // 音乐名
-        type: "netease", // 平台
-        filter: 'name',
-        page: 1
+async function getMusic(data, mode = "name", type = "netease", page = 1) {
+    const res = await axios.post(config.url, {
+        input: data,
+        type: type,
+        filter: mode,
+        page: page
     }, {
         headers: {
             'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
             'X-Requested-With': 'XMLHttpRequest',
-            // 'Referer': `https://www.yueting.net/tool/music/?name=${encodeURIComponent(name)}&type=netease`,
-            // 'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/137.0.0.0 Safari/537.36'
+
+            ...config.web_ua === ""
+                ? {}
+                : { "User-Agent": config.web_ua },
+
+            ...config.web_referer
+                ? { "Referer": `${config.url}?${mode}=${encodeURIComponent(data)}&type=${type}&page=${page}` }
+                : {}
         }
     })
 
-    logger.warn(JSON.stringify(data, (key, value) => {
+
+    logger.warn(JSON.stringify(res, (key, value) => {
         if (key === 'request' || key === 'config' || key === 'headers') return undefined;
         if (typeof value === 'bigint') return value.toString();
         return value;
     }, 4))
-    return data.data.data;
+    return res.data.data;
 }
+/*
+
+curl -X POST 'https://www.yueting.net/tool/music/' \
+-H 'Content-Type: application/x-www-form-urlencoded; charset=UTF-8' \
+-H 'X-Requested-With: XMLHttpRequest' \
+-H 'User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/137.0.0.0 Safari/537.36' \
+
+--data-urlencode 'input=are you lost'  \
+--data-urlencode 'filter=name'  \
+--data-urlencode 'type=netease'  \
+--data-urlencode 'page=1'
+
+--data-urlencode 'input=https://music.163.com/#/song?id=39227633' \
+--data-urlencode 'filter=url' \
+--data-urlencode 'type=netease' \
+--data-urlencode 
+--data-urlencode 
+
+[
+    {
+        "type": "",
+        "link": "",
+        "songid": 39227633,
+        "title": "His Theme",
+        "author": "Toby Fox",
+        "lrc": "",
+        "url": "http://music.163.com/song/media/outer/url?id=39227633.mp3",
+        "pic": "http://p1.music.126.net/mQcab-6L7D-w1lRxmYB7MQ==/109951168015051713.jpg?param=300x300"
+    }
+]
+
+*/
+
+
+
